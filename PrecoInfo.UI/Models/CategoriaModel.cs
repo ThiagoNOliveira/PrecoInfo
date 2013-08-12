fcb029c9ac793f43acaf1a9440a99ab80cb6de5e
ThiagoNOliveira <eno.thiago@gmail.com>
2013-08-12 13:07:08 -0300

    Lista de categoria e subcategoria

---
+++ PrecoInfo.UI/Models/CategoriaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrecoInfo.UI.Models
+{
+    public class CategoriaModel
+    {
+        public string Nome { get; set; }
+        public string URL { get; set; }
+        public string Icone { get; set; }
+        public IList<String> Subcategorias { get; set; } 
+    }
+}
