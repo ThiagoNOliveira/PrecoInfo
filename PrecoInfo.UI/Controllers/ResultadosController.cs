e75075f88a092c7de841d7f23b619bebc98cff62
Isaac Garcia <isaac@workker.com.br>
2013-11-02 16:59:26 -0200

    Página de resultados

---
+++ PrecoInfo.UI/Controllers/ResultadosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrecoInfo.UI.Controllers
+{
+    public class ResultadosController : Controller
+    {
+        //
+        // GET: /Resultados/
+
+        public ActionResult Index(Guid id)
+        {
+
+            return View();
+        }
+
+    }
+}
